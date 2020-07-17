bbc0d92a40c8a1e5690bf1073f146a205c3c2b63
Devesh Tyagi <Tyagi77@outlook.com>
2020-07-17 16:39:17 +0530

    Completed

---
+++ index.js
@@ -0,0 +1,20 @@
+
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource = "images/dice" + randomNumber1 + ".png";
+
+document.querySelector(".img1").setAttribute("src",randomImageSource);
+
+document.querySelector(".img2").setAttribute("src","images/dice"+randomNumber2+".png");
+
+
+if(randomNumber1> randomNumber2){
+  document.querySelector("h1").innerText = "🚩Player 1 Wins"
+}
+else if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerText = "Player 2 Wins 🚩"
+}else{
+  document.querySelector("h1").innerText = "Draw"
+}
